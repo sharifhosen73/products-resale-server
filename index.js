b7841f59d1e3e16129e81ebfa8f3cbc229a0d42e
Sharif <sharifhosen73@gmail.com>
2022-11-24 11:48:35 +0600

    single product added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +25,14 @@ async function run() {
       const query = {};
       const products = await productsCollection.find(query).toArray();
       res.send(products);
+    });
+
+    // single product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
     });
   } finally {
   }
